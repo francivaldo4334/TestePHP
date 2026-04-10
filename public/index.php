b4211272de1fd7e1d5e7bca3881ae545547a547f
francivaldo4334 <francivaldodev@gmail.com>
2026-04-09 22:39:00 -0300

    :sparkles: feat: implementa classe de view responsavel por renderizar a pagina

--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 
-echo "Tudo ok!";
+require __DIR__ . '/../vendor/autoload.php';
+
+use App\Http\Router;
+use App\Views\View;
+
+$router = new Router();
+
+$router->get('/', function(){
+    return View::render('base', [
+        'name'=>"Nome"
+    ]);
+});
+
+$router->run();
