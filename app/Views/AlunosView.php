f443ae9dd41b0439aef8a1580a822314d4b178a9
francivaldo4334 <francivaldodev@gmail.com>
2026-04-10 12:18:04 -0300

    save

--- app/Views/AlunosView.php
+++ app/Views/AlunosView.php
@@ -5,6 +5,11 @@ namespace App\Views;
 class AlunosView extends View {
     public static function render($view, $vars = []) {
         return SidebarLayoutView::render('layouts/crud', [
+            'btn_create'=> View::render('components/button', [
+                'content'=>'Adcionar Aluno',
+                'class'=> 'btn-primary',
+                'onclick'=>'id_modal_create.showModal()'
+            ]),
             'list_content' => TableView::render(
                 [
                     ['title' => 'Nome'], 
@@ -26,7 +31,36 @@ class AlunosView extends View {
                     ['data' => $item['create_at']],
                     ['data' => $item['class_name']],
                 ],
-            )
+            ),
+            'form_create_content'=> FormView::render(
+                [
+                   [
+                       'placeholder' => 'Nome do aluno',
+                       'value' => '',
+                       'label' => 'Nome',
+                       'name' => 'name',
+                   ],
+                   [
+                       'placeholder' => 'E-mail do aluno',
+                       'value' => '',
+                       'label' => 'Email',
+                       'type' => 'email',
+                       'name' => 'email',
+                   ],
+                ],
+                [
+                   [
+                      'label' => "Turma",
+                      'options' => ListView::render('components/option', [
+                              [
+                                  'label' => "Item 1",
+                                  'value' => 0,
+                              ]
+                      ])
+                   ] 
+                ],
+                []
+            ),
         ], 'alunos');
     }
 }
---
+++ app/Views/FormView.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Views;
+
+class FormView extends View {
+    public static function render(
+        $inputs = [],
+        $selects = [],
+        $actions = [],
+    )
+    {
+    	return parent::render('components/form', [
+	       'content'=> ListView::render(
+	           'components/label',
+	           array_map(fn($item)=>['label'=>$item['label'], 'content'=>View::render('components/input', $item)], $inputs)
+	       ) . ListView::render(
+	            'components/label',
+                array_map(fn($item) => ['label' => $item['label'], 'content' => View::render('components/select', $item)], $selects)
+           ) . ListView::render('components/button', $actions),
+	    ]);
+    }
+}
